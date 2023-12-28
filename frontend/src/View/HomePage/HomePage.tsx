beaf0fe6cb3afa601e19c10fc0be7091437976d5
Gabriel <brysiekholub@gmail.com>
2023-12-28 12:57:17 +0100

    feat(frontend): add navbar

--- frontend/src/View/HomePage/HomePage.tsx
+++ frontend/src/View/HomePage/HomePage.tsx
@@ -1,5 +1,12 @@
+import { Navbar } from '../../Component/Navigation'
+
 const Component = () => {
-  return <div>Main page home</div>
+  return (
+    <>
+      <Navbar />
+      <div>Main page home</div>
+    </>
+  )
 }
 
 export default Component
